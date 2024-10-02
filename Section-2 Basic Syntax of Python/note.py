53071ee80a422251a6b79a25730d3fa72a6919da
Mishkat Hossain <hossainmishkat0@gmail.com>
2024-10-02 08:49:16 +0600

    alice and bob

---
+++ Section-2 Basic Syntax of Python/note.py
@@ -0,0 +1,110 @@
+# First Python Program
+
+print('Hello World')
+
+
+# Python Terminal/ Python Console. Training. Doesn't look visual but for beginner output.
+
+# python sequence of work flow and alignment. 
+
+# print: output function, to show output
+
+print("This is line one") 
+print("This is line two")
+print("This is line three")
+print("This is line four")
+print("This is line five")
+print("This is line six")
+print("This is line seven")
+
+# I will discuss more about functions/Builtin functions later
+
+
+# Variables and Data type
+# Variables. Like a container cup. You can keep anything, even variable itself. 
+# Remember find the value of x in Highschool? x was a variable. x + y = 10, x - y = 5
+
+x = 10
+a = 12
+c = 13677
+b = a
+
+# Data types. For now [string, number, boolean]
+# Python is dynamically typed. You don't have to specify data type
+
+# String/Text types
+
+x = "This is text line string"
+u = "T"
+k = "This is text line string"
+f = "  "
+f = " "
+
+# Integer type
+y = 10 #int  
+m = 23 #int
+l = 82 #int
+
+#float type
+w = 10.601290 #float
+w = 10.00 #float
+w = 10.9999999 #float
+
+
+#bool type
+a = True #bool 
+k = False #bool
+
+
+# Truethy consideration
+a = True #truthy value
+a = 1000 #truthy value
+a = 5.5 #truthy value
+a = 1 #truthy value
+a = "Machine Learning" #truthy value
+a = "LLM" #truthy value
+a = " " #truthy
+# any non zero 
+# any non empty data structure
+# any non string data structure
+
+
+# Falsy considerating
+b = False
+b = 0
+b = 0.0000
+b = ""
+
+#python identifier
+# identifiers are used for identification of variables, functions, classes, modules, objects
+
+# Rules for python identifier declaration
+# (a-z,A-Z,0-9, _)
+# must start with letter. Also _
+# Can not start with digit
+# case sensitive. mishkat & MISHKAT & Mishkat & MishkaT & misHKat all are different
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#output in console
+print('1')
+
+
+
+
